0defb19b29fb2c342a036febd6f4e5a00f6676bb
bitdern <acnewell09@gmail.com>
2023-01-24 18:10:06 -0600

    more component organization

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,48 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
-  const handleClick = () => {
-    console.log("hiya, derrrrrrrn");
+  const [blogs, setBlogs] = useState([
+    {
+      title: "blog one",
+      body: "this is where text will go",
+      topic: "Beef Initiative",
+      id: 1,
+    },
+    {
+      title: "blog two",
+      body: "this is where text will go",
+      topic: "Beef Initiative",
+      id: 2,
+    },
+    {
+      title: "blog three",
+      body: "this is where text will go",
+      topic: "Bitcoin",
+      id: 3,
+    },
+  ]);
+
+  const [name, setName] = useState("dern daddu");
+
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
   };
+
+  useEffect(() => {
+    console.log("use effect ran");
+  }, []);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click here</button>
+      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.topic === "Bitcoin")}
+        title="Bitcoin Blogs"
+      />
+      <button onClick={() => setName("dernpapi")}>change name</button>
+      <p>{name}</p>
     </div>
   );
 };
